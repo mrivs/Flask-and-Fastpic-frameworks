6b23163b56ec40e3e6adfd0091a84e7945a1be5a
mrivs <valentinilyins@gmail.com>
2024-01-15 19:57:33 +0300

    task end

--- homework01/task01.py
+++ homework01/task01.py
@@ -1,18 +1,11 @@
 """
-Создать базовый шаблон для всего сайта, содержащий
-общие элементы дизайна (шапка, меню, подвал), и
-дочерние шаблоны для каждой отдельной страницы.
-Например, создать страницу "О нас" и "Контакты",
-используя базовый шаблон.
+Задание
+Создать базовый шаблон для интернет-магазина, содержащий общие элементы дизайна (шапка, меню, подвал), и дочерние шаблоны для страниц категорий товаров и отдельных товаров. Например, создать страницы «Одежда», «Обувь» и «Куртка», используя базовый шаблон.
 
 """
 from flask import Flask
 from flask import render_template 
 
-html = """
-<h1>Моя первая HTML страница</h1>
-<p>Привет, мир!</p>
-"""
 app = Flask(__name__)
 
 @app.route("/")
@@ -29,6 +22,26 @@ def about_():
 def contacts_():
     context={"title":"Контакты"}
     return render_template('contacts.html', **context)
+
+@app.route('/clothes/')
+def clothes_():
+    context={"title":"Одежда"}
+    return render_template('clothes.html', **context)
+
+@app.route('/shoes/')
+def shoes_():
+    context={"title":"Обувь"}
+    return render_template('shoes.html', **context)
+
+@app.route('/coat/')
+def coat_():
+    context={"title":"Куртка"}
+    return render_template('coat.html', **context)
+
+@app.route('/jacket/')
+def jacket_():
+    context={"title":"Пиджак"}
+    return render_template('jacket.html', **context)
 
 if __name__ == "__main__":
     app.run()
